8357d60a6015e7ebd89f0ab9916b0d9af41e26ac
hermtechs <hermtechs@gmail.com>
2024-02-18 12:54:43 +0300

    _

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,15 @@ const closeCartBtn = document.querySelector(".close-cart-btn");
 const shoppingCartMainContainer = document.querySelector(
   ".shopping-cart-container"
 );
+const openCartBtn = document.querySelector(".cart-count-icon");
+
 closeCartBtn.addEventListener("click", () => {
   shoppingCartMainContainer.classList.add("close-entire-cart");
+});
+openCartBtn.addEventListener("click", () => {
+  if (shoppingCartMainContainer.classList.contains("close-entire-cart")) {
+    shoppingCartMainContainer.classList.remove("close-entire-cart");
+  } else {
+    shoppingCartMainContainer.classList.add("close-entire-cart");
+  }
 });
