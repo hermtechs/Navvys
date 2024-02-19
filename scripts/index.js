45f16f61f89d0201626e2def59b912a1bb1ba001
hermtechs <hermtechs@gmail.com>
2024-02-19 04:37:10 +0300

    added licenses overlay

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,8 @@ const shoppingCartMainContainer = document.querySelector(
   ".shopping-cart-container"
 );
 const openCartBtn = document.querySelector(".cart-count-icon");
+const closeLicensesOverlayBtn = document.querySelector(".close-overlay-btn");
+const licensesOverlay = document.querySelector(".licenses-overlay");
 
 closeCartBtn.addEventListener("click", () => {
   shoppingCartMainContainer.classList.add("close-entire-cart");
@@ -15,3 +17,44 @@ openCartBtn.addEventListener("click", () => {
     shoppingCartMainContainer.classList.add("close-entire-cart");
   }
 });
+closeLicensesOverlayBtn.addEventListener("click", () => {
+  licensesOverlay.style.display = "none";
+});
+
+const data = [
+  {
+    id: "1",
+    beatTitle: "Beat and song1",
+    price: 30,
+    audioUrl: "/audios/sample_audio.mp3",
+    licence: "premium",
+  },
+  {
+    id: "1",
+    beatTitle: "Beat and song1",
+    price: 30,
+    audioUrl: "/audios/sample_audio.mp3",
+    licence: "basic",
+  },
+  {
+    id: "1",
+    beatTitle: "Beat and song1",
+    price: 30,
+    audioUrl: "/audios/sample_audio.mp3",
+    licence: "premium",
+  },
+  {
+    id: "1",
+    beatTitle: "Beat and song1",
+    price: 30,
+    audioUrl: "/audios/sample_audio.mp3",
+    licence: "unlimitted",
+  },
+  {
+    id: "1",
+    beatTitle: "Beat and song1",
+    price: 30,
+    audioUrl: "/audios/sample_audio.mp3",
+    licence: "exclusive",
+  },
+];
