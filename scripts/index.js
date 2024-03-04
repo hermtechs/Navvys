ba7121e232417a4bc8d2647d93c2fd1d74864c6e
hermtechs <hermtechs@gmail.com>
2024-03-04 04:44:02 +0300

    added all the math necessary for the shopping cart to work

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,8 @@ const licensesContainer = document.querySelector(".licenses-container");
 const closeAudioPlayerBtn = document.querySelector(".close-audio-player-btn");
 const audioPlayer = document.querySelector(".audio-player-container");
 const beatsContainer = document.querySelector(".beats-container");
+const cartCountElement = document.querySelector(".shopping-cart-count");
+const cartContainer = document.querySelector(".all-cart-items-wrapper");
 
 closeCartBtn.addEventListener("click", () => {
   shoppingCartMainContainer.classList.add("close-entire-cart");
@@ -17,7 +19,6 @@ openCartBtn.addEventListener("click", openShoppingCart);
 
 closeAudioPlayerBtn.addEventListener("click", () => {
   audioPlayer.classList.add("closing-audio-player");
-  console.log("clicked");
 });
 
 const beatsData = [
@@ -94,6 +95,7 @@ function openShoppingCart() {
   }
 }
 
+//open licenses overlay
 function openLicensesOverlay(event) {
   licensesContainer.style.display = "grid";
   // console.log(beatsData);
@@ -104,7 +106,7 @@ function openLicensesOverlay(event) {
     return data.id === clickedProductId;
   });
 
-  // console.log(clickedProductData);d
+  // console.log(clickedProductData);
   const {
     id,
     beatTitle,
@@ -167,19 +169,16 @@ function openLicensesOverlay(event) {
   </div>
   `;
 
-  // console.log(clickedProductData.beatTitle);
-
   licensesOverlay.innerHTML = licensesOverlayinnerHTML;
 
   //adding click event listener to add to cart btns
   const addToCartBtn = document.querySelectorAll(".license-specific-price");
   addToCartBtn.forEach((btn) =>
     btn.addEventListener("click", (event) => {
-      openShoppingCart();
+      // openShoppingCart();
       //add Item to cart
       addItemToCart(event);
       licensesContainer.style.display = "none";
-      console.log("clicked");
     })
   );
 
@@ -190,11 +189,61 @@ function openLicensesOverlay(event) {
   });
 }
 
+// add selected item to cart based on licence selected
+function addItemToCart(event) {
+  // console.log(event.currentTarget);
+  const clickedItemId = event.currentTarget.id;
+  // console.log(clickedItemId);
+  const licenseType = event.currentTarget.dataset.license;
+  // console.log(clickedItemId, licenseType);
+  const clickedItem = beatsData.find((item) => {
+    return item.id === clickedItemId;
+  });
+
+  const {
+    id,
+    beatTitle,
+    albumArtUrl,
+    audioUrl,
+    license_basic_price,
+    license_premium_price,
+    license_exclusive_price,
+  } = clickedItem;
+  // console.log(clickedItem);
+
+  const selectedLicense = event.currentTarget.dataset.license;
+
+  const cartItemHTML = `
+  <div class="cart-items-container">
+  <div class="cart-image" data-audiourl="${audioUrl}">
+   <img src="${albumArtUrl}" alt="">
+  </div>
+  <p class="product-title">${beatTitle}</p> 
+   <h5 class="product-price">${
+     selectedLicense === "basic"
+       ? license_basic_price
+       : selectedLicense === "premium"
+       ? license_premium_price
+       : license_exclusive_price
+   }</h5>
+  <i class="fas fa-times remove-cart-item-btn"></i>
+ </div> 
+  `;
+  cartContainer.innerHTML += cartItemHTML;
+  updateCartCount();
+  calculateCartTotal();
+  const removeItemBtn = document.querySelectorAll(".remove-cart-item-btn");
+
+  removeItemBtn.forEach((btn) =>
+    btn.addEventListener("click", removeItemFromCart)
+  );
+}
+
 //generating products (items) from our CMS dataset
 function getBeats() {
   const beats = beatsData.map((item) => {
     const { id, beatTitle, audioUrl, license_basic_price } = item;
-    // console.log(id);
+
     return `
     <div class="beat-container" id="${id}">
     <div class="play-btn-container" beatsData-audioUrl="${audioUrl}">
@@ -224,21 +273,45 @@ function getBeats() {
 }
 getBeats();
 
-function addItemToCart(event) {
-  console.log(event.currentTarget);
-  const clickedItemId = event.currentTarget.id;
-  const licenseType = event.currentTarget.dataset.license;
-  console.log(clickedItemId, licenseType);
-}
-
 //calculating cart total
 function calculateCartTotal() {
   const shoppingCart = document.querySelector(".cart");
   const allProductsInCart = shoppingCart.querySelectorAll(
     ".cart-items-container"
   );
-
   // console.log(allProductsInCart);
+  let cartTotal = 0;
+  allProductsInCart.forEach((item) => {
+    const itemPrice = item
+      .querySelector(".product-price")
+      .textContent.replace("$", "");
+    // console.log(itemPrice);
+    const itemPriceValue = parseInt(itemPrice);
+    cartTotal += itemPriceValue;
+  });
+  // console.log(cartTotal);
+  const cartTotalElement = document.querySelector(".total-price-value");
+  // console.log(cartTotalElement);
+  cartTotalElement.innerText = cartTotal;
 }
-
 calculateCartTotal();
+
+//updating cart count
+function updateCartCount() {
+  const shoppingCart = document.querySelector(".cart");
+  const allProductsInCart = shoppingCart.querySelectorAll(
+    ".cart-items-container"
+  );
+  const cartCountElement = document.querySelector(".shopping-cart-count");
+  cartCountElement.innerText = allProductsInCart.length;
+}
+updateCartCount();
+
+//remove item from cart
+function removeItemFromCart(event) {
+  const clickedItem = event.currentTarget.parentElement;
+  clickedItem.remove();
+  calculateCartTotal();
+  updateCartCount();
+  console.log("item removed");
+}
